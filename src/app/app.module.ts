9b6a993293eee7d38208cc2bf3b9d9fe5a414662
mfrancavigliaeximie <francaviglia.m@node.coop>
2022-07-10 22:14:16 +0200

    Aggiunta ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        FormsModule
+        FormsModule,
+        StoreModule.forRoot({}, {})
     ],
   providers: [],
   bootstrap: [AppComponent]
