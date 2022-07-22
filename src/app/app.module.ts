becfb6a682b1c73556d44dff9b2a1f5f298b1034
mfrancavigliaeximie <francaviglia.m@node.coop>
2022-07-22 16:31:08 +0200

    Installazione store dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {EffectsModule} from "@ngrx/effects";
 import {HeroEffect} from "./effects/hero.effect";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -26,7 +27,11 @@ import {StoreDevtoolsModule} from "@ngrx/store-devtools";
     HttpClientModule,
     StoreModule.forRoot({heroState: heroesReducer}, {}),
     EffectsModule.forRoot([HeroEffect]),
-    StoreDevtoolsModule
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
